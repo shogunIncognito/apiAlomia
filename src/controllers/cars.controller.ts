a52cccf1c9a9ee1999f9791f3f06adcdf2a65b24
shogunIncognito <saidsamuel2006@hotmail.com>
2024-05-19 17:07:15 -0500

    feat: agregando transacciones a cars

--- src/controllers/cars.controller.ts
+++ src/controllers/cars.controller.ts
@@ -1,10 +1,25 @@
 import { Request, Response } from "express"
 import { Car } from "../entities/Car"
 import { brands } from "../constants/const"
+import { Transaction } from "../entities/Transaction"
+
+export const getTransactionsByCarId = async (carId: { _id: number }) => {
+    return Transaction.findBy({ car: { _id: carId._id } });
+}
 
 export const getCars = async (_req: Request, res: Response) => {
     const cars = await Car.find()
-    return res.json(cars)
+
+    const carsWithTransactions = await Promise.all(cars.map(async (car) => {
+        const transactions = await getTransactionsByCarId(car);
+        // Aquí realizamos una consulta para obtener las transacciones asociadas a este auto
+        return {
+            ...car,
+            transactions: transactions?.map(transaction => transaction._id) || []
+        };
+    }));
+
+    return res.json(carsWithTransactions);
 }
 
 export const getCar = async (req: Request, res: Response) => {
@@ -41,7 +56,7 @@ export const deleteCar = async (req: Request, res: Response) => {
 
 export const updateCarImages = async (req: Request, res: Response) => {
     const { id } = req.params
-    const {images} = req.body
+    const { images } = req.body
 
     const car = await Car.findOneBy({ _id: parseInt(id) })
     if (!car) return res.status(404).json({ message: 'Car not found' })
